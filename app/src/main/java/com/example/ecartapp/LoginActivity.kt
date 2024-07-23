76f7962152d8a3181d3e1f704e2ab8e4db85e692
AshokSannala <sannala786mhmd@gmail.com>
2024-07-23 09:32:34 -0500

    ECart initial pushing

---
+++ app/src/main/java/com/example/ecartapp/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.example.ecartapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ecartapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, DashBoardActivity::class.java)
+            startActivity(intent)
+
+        }
+
+    }
+}
